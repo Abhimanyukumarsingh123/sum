c04b40ad0a4570ebcb656c0d082208d8a91d69b1
abhimanyukumarsingh123 <akturki11122004@gmail.com>
2023-05-04 01:42:00 -0700

    frist commit

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i=1,sum=0;
+    cout<<"Enter a Number :"<<endl;
+    cin>>n;
+    for( ;i<=n ; i++){
+        sum+=i;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
